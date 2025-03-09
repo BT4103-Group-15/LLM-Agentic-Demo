ac440a90babdaebe38f57c815b08cb20e078b361
Choon Leng <73415759+ChoonLeng773@users.noreply.github.com>
2025-03-09 19:24:51 +0800

    completed email to scoping sheet in markdown generator

--- agentic_demo_crewai/main.py
+++ agentic_demo_crewai/main.py
@@ -3,6 +3,9 @@ Fast API server to use the local LLM to generate and return the scoping sheet
 """
 
 from fastapi import FastAPI
+import pandas as pd
+
+# from fastapi.middleware.cors import CORSMiddleware
 
 # from fastapi.responses import Response -> for markdown
 from schemas import RequirementRequest
@@ -14,15 +17,34 @@ app = FastAPI(
     description="API endpoint to be connected via N8N http request node",
 )
 
+
 # For returning in markdown format
 # return Response(content=markdown_content, media_type="text/markdown")
 
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI is running successfully!"}
+
+
+def transform_input(input_json):
+    """Converts input JSON to match FastAPI schema"""
+    return {
+        "email_text": input_json["email"],
+        "requirement_df": [
+            {"category": item["Category"], "requirement": item["Requirement"]}
+            for item in input_json["requirement_columns"]
+        ],
+    }
+
+
 @app.post("/api/scoping-local-flow")
-async def ask_agent(request: RequirementRequest):
+async def ask_agent(request: RequirementRequest):  # raw request
     """Endpoint to interact with the local LLM"""
+    # fixed_request = transform_input(request)  # Fix structure
+    # request_obj = RequirementRequest(**fixed_request)
     response = run_scoping_generator(request.email_text, request.requirement_df)
-    return {"response": response}
+    return response  # {"response": response}
 
 
 # Run the FastAPI app from this file
