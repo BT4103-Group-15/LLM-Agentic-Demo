daf2eeb3eb4fe902d9d88535ae1a9c998735ff5a
ileanagoh <ileanagoh@hotmail.com>
2025-03-21 15:58:01 +0800

    edit chatbot_logs migration file

--- internal-staff-app/migrations/04_create_table_chatbot_logs.sql
+++ internal-staff-app/migrations/04_create_table_chatbot_logs.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS chatbot_logs (
     log_id INT AUTO_INCREMENT PRIMARY KEY,
     pentest_request_id INT,
-    user_input JSON,
-    bot_response JSON,
+    chat_history JSON,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (pentest_request_id) REFERENCES pentest_requests(pentest_request_id)
 );
